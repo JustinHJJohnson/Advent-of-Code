67893bb6212197bd368c4a508e7c49da2d07c1ff
Justin <justin.h.j.johnson@gmail.com>
2020-12-12 13:40:37 +1100

    Finished day 3

--- AdventOfCode/Day3Part2/Day3Part2.c
+++ AdventOfCode/Day3Part2/Day3Part2.c
@@ -1,68 +1,56 @@
 #include <stdio.h>
 
 /*
---- Day 3: Toboggan Trajectory ---
+--- Part Two ---
 
-With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
 
-Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
 
-..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#
+	Right 1, down 1.
+	Right 3, down 1. (This is the slope you already checked.)
+	Right 5, down 1.
+	Right 7, down 1.
+	Right 1, down 2.
 
-These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
 
-..##.........##.........##.........##.........##.........##.......  --->
-#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
-.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
-..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
-.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
-..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
-.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
-.#........#.#........#.#........#.#........#.#........#.#........#
-#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
-#...##....##...##....##...##....##...##....##...##....##...##....#
-.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
 
-You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
-
-The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
-
-From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
-
-The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
-
-..##.........##.........##.........##.........##.........##.......  --->
-#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
-.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
-..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
-.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
-..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
-.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
-.#........#.#........X.#........#.#........#.#........#.#........#
-#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
-#...##....##...##....##...#X....##...##....##...##....##...##....#
-.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
-
-In this example, traversing the map using this slope would cause you to encounter 7 trees.
-
-Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
-
-Your puzzle answer was 193.
+Your puzzle answer was 1355323200.
 */
+
 typedef struct Position {
 	int x;
 	int y;
 } Position;
+
+int numTreesEncountered(char* input[], int rightMove, int downMove, int width, int height)
+{
+	Position currentPos = { 0, 0 };		//current position in the grid
+	int treeCount = 0;					//number of trees encountered
+
+	//Keep looping until the botton of the grid
+	while (currentPos.y + downMove < height)
+	{
+		//If the next move is greater than width, reduce it by width to simulate infinite repeating to the right
+		if (currentPos.x + rightMove >= width)
+		{
+			currentPos.x += rightMove - width;
+		}
+		else
+		{
+			currentPos.x += rightMove;
+		}
+
+		currentPos.y += downMove;
+
+		if (input[currentPos.y][currentPos.x] == '#') treeCount++;
+	}
+
+	printf("Encountered %d trees.\n", treeCount);
+	return treeCount;
+}
 
 int main(int argc, char* argv[])
 {
@@ -395,28 +383,15 @@ int main(int argc, char* argv[])
 
 	const int width = strlen(input[0]);					//Width of the grid
 	const int height = sizeof(input) / sizeof(char*);	//Height of the grid
-	const int rightMove = 3;							//How far to move right each time
-	const int downMove = 1;								//How far to move down each time
-	Position currentPos = { 0, 0 };						//Current position in the grid
-	int treeCount = 0;									//Number of trees encountered
+	int treeCountTotal = 0;								//Total number of trees encountered over multiple runs
 
-	//Keep looping until the botton of the grid
-	while (currentPos.y + downMove < height)
-	{
-		//If the next move is greater than width, reduce it by width to simulate infinite repeating to the right
-		if (currentPos.x + rightMove >= width)
-		{
-			currentPos.x += rightMove - width;
-		}
-		else
-		{
-			currentPos.x += rightMove;
-		}
+	int run1 = numTreesEncountered(input, 1, 1, width, height);
+	int run2 = numTreesEncountered(input, 3, 1, width, height);
+	int run3 = numTreesEncountered(input, 5, 1, width, height);
+	int run4 = numTreesEncountered(input, 7, 1, width, height);
+	int run5 = numTreesEncountered(input, 1, 2, width, height);
 
-		currentPos.y += downMove;
+	treeCountTotal = run1 * run2 * run3 * run4 * run5;
 
-		if (input[currentPos.y][currentPos.x] == '#') treeCount++;
-	}
-
-	printf("Encountered %d trees.\n", treeCount);
+	printf("Encountered %d trees in total.\n", treeCountTotal);
 }
--- InputFormatTo2DArray.py
+++ InputFormatTo2DArray.py
@@ -1,4 +1,4 @@
-inputFile = open("Day 2 Input.txt", "r+")
+inputFile = open("input.txt", "r+")
 for line in inputFile:
     #inputFile.write('\"' + line.strip('\n') + '\",')
     print('\"' + line.strip('\n') + '\",')
